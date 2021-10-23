dddaebc6edded2c0939268b8a1cf902fdacf18ae
01Blu3 <79278037+01Blu3@users.noreply.github.com>
2021-10-23 15:48:56 -0500

    Front end (#1)
    * Created Nav-Bar
    
    * final

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ app.config['SECRET_KEY'] = '2d6ca153ea201fe4daf5a90f380026b5'
 def home():
     return render_template('home.html')
 
+@app.route("/finder")
+def finder():
+    return render_template('cFinder.html')
+
+@app.route("/about-us")
+def about():
+    return render_template('about-us.html')
+
+@app.route("/toolbox")
+def toolbox():
+    return render_template('toolbox.html')
+
 # Main Function
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
